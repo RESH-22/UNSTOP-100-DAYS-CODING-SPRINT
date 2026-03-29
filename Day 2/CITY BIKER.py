5c5986030c3981a73539b61006b564b91beb24bd
RESHMITHA R <rreshmitha22@gmail.com>
2026-03-29 09:47:14 +0530

    Implement highestAltitude function in CITY BIKER.py

---
+++ Day 2/CITY BIKER.py
@@ -0,0 +1,16 @@
+def highestAltitude(n, arr):
+    current_altitude = 0
+    max_altitude = 0
+    
+    for gain in arr:
+        current_altitude += gain
+        max_altitude = max(max_altitude, current_altitude)
+    
+    return max_altitude
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = highestAltitude(n, arr)
+    print(result)
