614b64fb75b5fd18d8d53aa5a65186fa6e0aac97
RESHMITHA R <rreshmitha22@gmail.com>
2026-03-31 15:57:02 +0530

    Add function to count set bits in binary representation

---
+++ Day 8/COUNT SET BITS.py
@@ -0,0 +1,10 @@
+def count_ones_in_binary(n):
+    total = 0
+    for i in range(1, n + 1):
+        total += i.bit_count()
+    return total
+
+if __name__ == "__main__":
+    n = int(input())
+    result = count_ones_in_binary(n)
+    print(result)
